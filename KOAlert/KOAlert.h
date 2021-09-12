11f75ca362c8a57a7d1f895449ab8af3f1662201
KaanOzdemir <kaan.ozdemir@tsoft.com.tr>
2021-09-12 17:35:51 +0300

    Initial Commit

---
+++ KOAlert/KOAlert.h
@@ -0,0 +1,18 @@
+//
+//  KOAlert.h
+//  KOAlert
+//
+//  Created by Kaan Ozdemir on 12.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KOAlert.
+FOUNDATION_EXPORT double KOAlertVersionNumber;
+
+//! Project version string for KOAlert.
+FOUNDATION_EXPORT const unsigned char KOAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KOAlert/PublicHeader.h>
+
+
